414a9918846427672b5f0c816caeb28cc6f82253
Tiagueira0081 <tiagofeliphe7@gmail.com>
2023-09-02 18:06:41 -0300

    aws

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portifolio.apps.PortifolioConfig'
+    'portifolio.apps.PortifolioConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -118,11 +119,29 @@ USE_I18N = True
 
 USE_TZ = True
 
+# AWS  Configuração
+
+AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.sa-east-1.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+AWS_LOCATION = 'static'
+AWS_QUERYSTRING_AUTH = False
+
+AWS_HEADRS = {
+    'Access-Control-Allow-Origin': '*'
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 FAVICON_URL = '/static/icons/favicon.ico'
 
 STATICFILES_DIRS = [
@@ -134,7 +153,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Configurando Midias
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
+MEDIA_URL = "https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('myportifolio/', admin.site.urls),
     path('', include('portifolio.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
