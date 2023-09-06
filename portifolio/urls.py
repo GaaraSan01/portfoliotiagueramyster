4f48c2f0a9799aee9416fcbaeca62a2bb8a89d55
Vinícius H <vini2003henri12395@gmail.com>
2023-09-05 21:19:49 -0300

    Testes

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -3,7 +3,7 @@ from portifolio.views import index, projetos, projeto, sobre, filtro
 
 urlpatterns = [
     path('', index, name='index'),
-    path('projetos/', projetos, name='projetos'),
+    path('projetosteste/', projetos, name='projetos'),
     path('projeto/<int:project_id>/', projeto, name='projeto'),
     path('sobre', sobre, name='sobre'),
     path('projetos/<str:category>', filtro, name='filtro')
